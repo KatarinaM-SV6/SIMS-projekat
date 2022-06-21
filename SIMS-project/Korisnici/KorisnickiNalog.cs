7576a69923fc4006a09a4e98b7417b57656a6c74
SelenaMilutin <selena.milutin@gmail.com>
2022-06-21 20:58:28 +0200

    Add classes for users and malfunction

---
+++ SIMS-project/Korisnici/KorisnickiNalog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_project.Korisnici
+{
+	public enum TipKorisnika { 
+		PRODAVAC,
+		ADMINISTRATOR,
+		REFERENT,
+		VODJA_STANICE
+	}
+	class KorisnickiNalog
+	{
+		string _korisnickoIme;
+		string _lozinka;
+		TipKorisnika _tipKorisnika;
+		Korisnik _korisnik;
+
+		public KorisnickiNalog() { }
+
+		public KorisnickiNalog(string korisnickoIme, string lozinka, TipKorisnika tipKorisnika, Korisnik korisnik)
+		{
+			_korisnickoIme = korisnickoIme;
+			_lozinka = lozinka;
+			_tipKorisnika = tipKorisnika;
+			_korisnik = korisnik;
+		}
+
+		public string KorisnickoIme { get => _korisnickoIme; set => _korisnickoIme = value; }
+		public string Lozinka { get => _lozinka; set => _lozinka = value; }
+		public TipKorisnika TipKorisnika { get => _tipKorisnika; set => _tipKorisnika = value; }
+		public Korisnik Korisnik { get => _korisnik; set => _korisnik = value; }
+	}
+}
---
+++ SIMS-project/Korisnici/Korisnik.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SIMS_project.NaplatneStanice;
+
+namespace SIMS_project.Korisnici
+{
+	class Korisnik
+	{
+		string _ime;
+		string _prezime;
+		NaplatnaStanica _radnoMesto;
+		KorisnickiNalog _korisnickiNalog;
+
+		public Korisnik()
+		{
+		}
+
+		public Korisnik(string ime, string prezime, KorisnickiNalog korisnickiNalog = null, NaplatnaStanica radnoMesto=null)
+		{
+			_ime = ime;
+			_prezime = prezime;
+			_radnoMesto = radnoMesto;
+			_korisnickiNalog = korisnickiNalog;
+		}
+
+		public string Ime { get => _ime; set => _ime = value; }
+		public string Prezime { get => _prezime; set => _prezime = value; }
+		internal NaplatnaStanica RadnoMesto { get => _radnoMesto; set => _radnoMesto = value; }
+		internal KorisnickiNalog KorisnickiNalog { get => _korisnickiNalog; set => _korisnickiNalog = value; }
+	}
+}
