5f15ed3bedcfb1d135f9c6a9d6f1d4c83e124523
unknown <spremic.katarina@gmail.com>
2022-06-21 21:27:12 +0200

    Add classes for price list and section

---
+++ SIMS-project/Deonice/Deonica.cs
@@ -0,0 +1,30 @@
+﻿using SIMS_project.NaplatneStanice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_project.Deonice
+{
+    class Deonica
+    {
+        int id;
+        int kilometraza;
+        NaplatnaStanica mestoUlaska;
+        NaplatnaStanica mestoIzlaska;
+
+        public Deonica(int id, int kilometraza, NaplatnaStanica mestoUlaska, NaplatnaStanica mestoIzlaska)
+        {
+            this.Id = id;
+            this.Kilometraza = kilometraza;
+            this.MestoUlaska = mestoUlaska;
+            this.MestoIzlaska = mestoIzlaska;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int Kilometraza { get => kilometraza; set => kilometraza = value; }
+        internal NaplatnaStanica MestoUlaska { get => mestoUlaska; set => mestoUlaska = value; }
+        internal NaplatnaStanica MestoIzlaska { get => mestoIzlaska; set => mestoIzlaska = value; }
+    }
+}
