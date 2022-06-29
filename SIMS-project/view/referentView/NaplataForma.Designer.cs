3d25b7669a368d89fb470490d4bba8ed619539f3
unknown <spremic.katarina@gmail.com>
2022-06-29 15:28:46 +0200

    Update design of NaplataForma

--- SIMS-project/view/referentView/NaplataForma.Designer.cs
+++ SIMS-project/view/referentView/NaplataForma.Designer.cs
@@ -29,9 +29,6 @@ namespace SIMS_project.view.referentView
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -66,7 +63,7 @@ namespace SIMS_project.view.referentView
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(38, 99);
+            this.label1.Location = new System.Drawing.Point(43, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(192, 20);
             this.label1.TabIndex = 0;
@@ -76,7 +73,7 @@ namespace SIMS_project.view.referentView
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(38, 399);
+            this.label2.Location = new System.Drawing.Point(42, 365);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(160, 20);
             this.label2.TabIndex = 1;
@@ -86,7 +83,7 @@ namespace SIMS_project.view.referentView
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(38, 509);
+            this.label3.Location = new System.Drawing.Point(43, 449);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(246, 20);
             this.label3.TabIndex = 2;
@@ -95,83 +92,83 @@ namespace SIMS_project.view.referentView
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(38, 35);
+            this.label4.Location = new System.Drawing.Point(43, 42);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(208, 20);
+            this.label4.Size = new System.Drawing.Size(239, 22);
             this.label4.TabIndex = 3;
             this.label4.Text = "Pristiglo vozilo sa tablicama: ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(38, 241);
+            this.label5.Location = new System.Drawing.Point(43, 219);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.Size = new System.Drawing.Size(120, 22);
             this.label5.TabIndex = 4;
             this.label5.Text = "Mesto ulaska:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(38, 282);
+            this.label6.Location = new System.Drawing.Point(43, 258);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(110, 20);
+            this.label6.Size = new System.Drawing.Size(124, 22);
             this.label6.TabIndex = 5;
             this.label6.Text = "Vreme ulaska:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(38, 329);
+            this.label7.Location = new System.Drawing.Point(43, 293);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(236, 20);
+            this.label7.Size = new System.Drawing.Size(268, 22);
             this.label7.TabIndex = 6;
             this.label7.Text = "Prosecna brzina kretanja (km/h):";
             // 
             // tbTablice
             // 
-            this.tbTablice.Location = new System.Drawing.Point(310, 35);
+            this.tbTablice.Location = new System.Drawing.Point(310, 39);
             this.tbTablice.Name = "tbTablice";
             this.tbTablice.ReadOnly = true;
-            this.tbTablice.Size = new System.Drawing.Size(157, 26);
+            this.tbTablice.Size = new System.Drawing.Size(174, 28);
             this.tbTablice.TabIndex = 7;
             // 
             // tbId
             // 
-            this.tbId.Location = new System.Drawing.Point(310, 93);
+            this.tbId.Location = new System.Drawing.Point(310, 66);
             this.tbId.Name = "tbId";
-            this.tbId.Size = new System.Drawing.Size(157, 26);
+            this.tbId.Size = new System.Drawing.Size(174, 28);
             this.tbId.TabIndex = 8;
             // 
             // tbMestoUlaska
             // 
-            this.tbMestoUlaska.Location = new System.Drawing.Point(310, 238);
+            this.tbMestoUlaska.Location = new System.Drawing.Point(249, 219);
             this.tbMestoUlaska.Name = "tbMestoUlaska";
             this.tbMestoUlaska.ReadOnly = true;
-            this.tbMestoUlaska.Size = new System.Drawing.Size(157, 26);
+            this.tbMestoUlaska.Size = new System.Drawing.Size(174, 28);
             this.tbMestoUlaska.TabIndex = 9;
             // 
             // tbVremeUlaska
             // 
-            this.tbVremeUlaska.Location = new System.Drawing.Point(310, 276);
+            this.tbVremeUlaska.Location = new System.Drawing.Point(249, 254);
             this.tbVremeUlaska.Name = "tbVremeUlaska";
             this.tbVremeUlaska.ReadOnly = true;
-            this.tbVremeUlaska.Size = new System.Drawing.Size(157, 26);
+            this.tbVremeUlaska.Size = new System.Drawing.Size(174, 28);
             this.tbVremeUlaska.TabIndex = 10;
             // 
             // tbBrzina
             // 
-            this.tbBrzina.Location = new System.Drawing.Point(373, 323);
+            this.tbBrzina.Location = new System.Drawing.Point(319, 290);
             this.tbBrzina.Name = "tbBrzina";
             this.tbBrzina.ReadOnly = true;
-            this.tbBrzina.Size = new System.Drawing.Size(94, 26);
+            this.tbBrzina.Size = new System.Drawing.Size(104, 28);
             this.tbBrzina.TabIndex = 11;
             // 
             // btnPokreni
             // 
-            this.btnPokreni.Location = new System.Drawing.Point(176, 134);
+            this.btnPokreni.Location = new System.Drawing.Point(167, 113);
             this.btnPokreni.Name = "btnPokreni";
-            this.btnPokreni.Size = new System.Drawing.Size(147, 42);
+            this.btnPokreni.Size = new System.Drawing.Size(155, 36);
             this.btnPokreni.TabIndex = 12;
             this.btnPokreni.Text = "Pokreni naplatu";
             this.btnPokreni.UseVisualStyleBackColor = true;
@@ -180,59 +177,59 @@ namespace SIMS_project.view.referentView
             // cbTip
             // 
             this.cbTip.FormattingEnabled = true;
-            this.cbTip.Location = new System.Drawing.Point(310, 396);
+            this.cbTip.Location = new System.Drawing.Point(249, 365);
             this.cbTip.Name = "cbTip";
-            this.cbTip.Size = new System.Drawing.Size(157, 28);
+            this.cbTip.Size = new System.Drawing.Size(174, 30);
             this.cbTip.TabIndex = 13;
             this.cbTip.SelectionChangeCommitted += new System.EventHandler(this.CbTip_SelectionChangeCommitted);
             // 
             // tbUplacenIznos
             // 
-            this.tbUplacenIznos.Location = new System.Drawing.Point(310, 506);
+            this.tbUplacenIznos.Location = new System.Drawing.Point(319, 441);
             this.tbUplacenIznos.Name = "tbUplacenIznos";
-            this.tbUplacenIznos.Size = new System.Drawing.Size(107, 26);
+            this.tbUplacenIznos.Size = new System.Drawing.Size(104, 28);
             this.tbUplacenIznos.TabIndex = 14;
             this.tbUplacenIznos.Leave += new System.EventHandler(this.TbUplacenIznos_Leave);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(38, 461);
+            this.label8.Location = new System.Drawing.Point(42, 405);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(163, 20);
+            this.label8.Size = new System.Drawing.Size(181, 22);
             this.label8.TabIndex = 15;
             this.label8.Text = "Iznos putarine (RSD):";
             // 
             // tbIznos
             // 
-            this.tbIznos.Location = new System.Drawing.Point(310, 455);
+            this.tbIznos.Location = new System.Drawing.Point(249, 399);
             this.tbIznos.Name = "tbIznos";
             this.tbIznos.ReadOnly = true;
-            this.tbIznos.Size = new System.Drawing.Size(107, 26);
+            this.tbIznos.Size = new System.Drawing.Size(174, 28);
             this.tbIznos.TabIndex = 16;
             // 
             // tbKusur
             // 
-            this.tbKusur.Location = new System.Drawing.Point(310, 556);
+            this.tbKusur.Location = new System.Drawing.Point(319, 489);
             this.tbKusur.Name = "tbKusur";
             this.tbKusur.ReadOnly = true;
-            this.tbKusur.Size = new System.Drawing.Size(107, 26);
+            this.tbKusur.Size = new System.Drawing.Size(104, 28);
             this.tbKusur.TabIndex = 17;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(38, 559);
+            this.label9.Location = new System.Drawing.Point(251, 495);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(54, 20);
+            this.label9.Size = new System.Drawing.Size(62, 22);
             this.label9.TabIndex = 18;
             this.label9.Text = "Kusur:";
             // 
             // btnZavrsi
             // 
-            this.btnZavrsi.Location = new System.Drawing.Point(176, 600);
+            this.btnZavrsi.Location = new System.Drawing.Point(167, 535);
             this.btnZavrsi.Name = "btnZavrsi";
-            this.btnZavrsi.Size = new System.Drawing.Size(147, 43);
+            this.btnZavrsi.Size = new System.Drawing.Size(166, 35);
             this.btnZavrsi.TabIndex = 19;
             this.btnZavrsi.Text = "Zavrsi naplatu";
             this.btnZavrsi.UseVisualStyleBackColor = true;
@@ -241,9 +238,9 @@ namespace SIMS_project.view.referentView
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(38, 199);
+            this.label10.Location = new System.Drawing.Point(42, 187);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(143, 20);
+            this.label10.Size = new System.Drawing.Size(166, 22);
             this.label10.TabIndex = 20;
             this.label10.Text = "Tablice sa papirica:";
             // 
@@ -251,47 +248,23 @@ namespace SIMS_project.view.referentView
             // 
             this.lblTablice.AutoSize = true;
             this.lblTablice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTablice.Location = new System.Drawing.Point(306, 199);
+            this.lblTablice.Location = new System.Drawing.Point(305, 185);
             this.lblTablice.Name = "lblTablice";
             this.lblTablice.Size = new System.Drawing.Size(0, 25);
             this.lblTablice.TabIndex = 21;
             // 
             // dataGridViewUredjaji
             // 
-            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridViewUredjaji.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
             this.dataGridViewUredjaji.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewUredjaji.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridViewUredjaji.DefaultCellStyle = dataGridViewCellStyle8;
-            this.dataGridViewUredjaji.Location = new System.Drawing.Point(643, 80);
+            this.dataGridViewUredjaji.Location = new System.Drawing.Point(654, 66);
             this.dataGridViewUredjaji.Name = "dataGridViewUredjaji";
             this.dataGridViewUredjaji.ReadOnly = true;
-            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridViewUredjaji.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
             this.dataGridViewUredjaji.RowHeadersWidth = 62;
             this.dataGridViewUredjaji.RowTemplate.Height = 28;
-            this.dataGridViewUredjaji.Size = new System.Drawing.Size(363, 259);
+            this.dataGridViewUredjaji.Size = new System.Drawing.Size(403, 158);
             this.dataGridViewUredjaji.TabIndex = 22;
             // 
             // Column1
@@ -313,16 +286,16 @@ namespace SIMS_project.view.referentView
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(686, 38);
+            this.label11.Location = new System.Drawing.Point(708, 16);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(0, 20);
+            this.label11.Size = new System.Drawing.Size(0, 22);
             this.label11.TabIndex = 23;
             // 
             // lblMesto
             // 
             this.lblMesto.AutoSize = true;
             this.lblMesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMesto.Location = new System.Drawing.Point(638, 52);
+            this.lblMesto.Location = new System.Drawing.Point(649, 38);
             this.lblMesto.Name = "lblMesto";
             this.lblMesto.Size = new System.Drawing.Size(259, 25);
             this.lblMesto.TabIndex = 24;
@@ -330,9 +303,9 @@ namespace SIMS_project.view.referentView
             // 
             // NaplataForma
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1050, 714);
+            this.ClientSize = new System.Drawing.Size(1080, 613);
             this.Controls.Add(this.lblMesto);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.dataGridViewUredjaji);
@@ -358,7 +331,7 @@ namespace SIMS_project.view.referentView
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "NaplataForma";
             this.Text = "Naplata Putarine";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUredjaji)).EndInit();
