36f7b949949f52876078d20a476b749cc9063d37
unknown <spremic.katarina@gmail.com>
2022-06-29 15:31:55 +0200

    Update design of GlavnaForma for Referent role

--- SIMS-project/view/referentView/GlavnaForma.Designer.cs
+++ SIMS-project/view/referentView/GlavnaForma.Designer.cs
@@ -39,7 +39,6 @@ namespace SIMS_project.view.referentView
             // 
             // menuStrip1
             // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.naplataToolStripMenuItem,
@@ -47,7 +46,7 @@ namespace SIMS_project.view.referentView
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
-            this.menuStrip1.Size = new System.Drawing.Size(1067, 37);
+            this.menuStrip1.Size = new System.Drawing.Size(668, 37);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -68,17 +67,17 @@ namespace SIMS_project.view.referentView
             // cbMesto
             // 
             this.cbMesto.FormattingEnabled = true;
-            this.cbMesto.Location = new System.Drawing.Point(55, 192);
-            this.cbMesto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbMesto.Location = new System.Drawing.Point(184, 127);
+            this.cbMesto.Margin = new System.Windows.Forms.Padding(4);
             this.cbMesto.Name = "cbMesto";
-            this.cbMesto.Size = new System.Drawing.Size(385, 33);
+            this.cbMesto.Size = new System.Drawing.Size(289, 33);
             this.cbMesto.TabIndex = 1;
             this.cbMesto.SelectionChangeCommitted += new System.EventHandler(this.CbMesto_SelectionChangeCommitted);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 138);
+            this.label1.Location = new System.Drawing.Point(97, 79);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(446, 25);
@@ -89,13 +88,13 @@ namespace SIMS_project.view.referentView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.ClientSize = new System.Drawing.Size(668, 394);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbMesto);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "GlavnaForma";
             this.Text = "Referent - Glavna Forma";
             this.menuStrip1.ResumeLayout(false);
