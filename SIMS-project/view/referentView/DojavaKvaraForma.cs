8f3490db3fe59214431ee86444d451ef30fe1853
unknown <spremic.katarina@gmail.com>
2022-06-29 13:30:19 +0200

    Fix bug in DojavaOKvaru form

--- SIMS-project/view/referentView/DojavaKvaraForma.cs
+++ SIMS-project/view/referentView/DojavaKvaraForma.cs
@@ -50,6 +50,7 @@ namespace SIMS_project.view.referentView
             if (izabranUredjaj != null)
             {
                 Program.dojaveRepo.Add(new Uredjaji.DojaveOKvaru.DojavaOKvaru(DateTime.Now, izabranUredjaj, tbOpis.Text, chbObustavi.Checked, ulogovaniReferent.Korisnik.RadnoMesto));
+                naplatnoMesto.UFunkciji = chbObustavi.Checked;
                 MessageBox.Show("Uspesno poslata dojava.");
             }
             else MessageBox.Show("Izaberite uredjaj.");
